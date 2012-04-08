959db972160947dcdf10fc58a9843b4931061373
JP Klein <jpklein@gmail.com>
2012-04-08 18:29:42 -0400

    Update Scrapbook & RGA theme to include Page Title

--- sites/all/themes/rga12/page.tpl.php
+++ sites/all/themes/rga12/page.tpl.php
@@ -38,6 +38,7 @@
 
       <div id="content" class="column">
         <a id="main-content"></a>
+        <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
         <?php print render($page['content']); ?>
       </div> <!-- /#content -->
 
